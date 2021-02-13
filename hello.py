7e59976d9bfd6ee02f54bd4c8514280fc7c98842
hansipeng <hansipeng@126.com>
2021-02-13 08:23:20 -0500

    2

--- hello.py
+++ hello.py
@@ -1,3 +1,23 @@
 #!/bin/env python3
 
-print("hello world! python3")
+import math
+
+k = 4
+w = 10
+d = math.sqrt(k ** 2 + w ** 2)
+print(d)
+
+for i in range(5):
+    print("hello world! python3.%d", i)
+    
+    
+print(""" this is first
+      this is second 
+      this is third
+      """)
+
+print("this is first \
+    this is second")
+a=5
+
+print(a, id(a))
